46cefac40962d0d7569d3138359813a54137a98d
nrLucas <56032832+nrLucas@users.noreply.github.com>
2020-08-20 22:35:22 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from xgboost import XGBClassifier
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -13,4 +14,67 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+    return data.drop(labels=self.columns, axis='columns')
+
+rm_columns.fit(X=df_data_1)
+df_data_2 = pd.DataFrame.from_records(
+    data=rm_columns.transform(
+        X=df_data_1
+    ),
+)
+
+rm_columns2.fit(X=df_data_2)
+df_data_3 = pd.DataFrame.from_records(
+    data=rm_columns2.transform(
+        X=df_data_2
+  ),
+)
+
+rm_columns3.fit(X=df_data_3)
+df_data_4 = pd.DataFrame.from_records(
+    data=rm_columns3.transform(
+        X=df_data_3
+   ),
+)
+
+rm_columns4.fit(X=df_data_4)
+df_data_5 = pd.DataFrame.from_records(
+    data=rm_columns4.transform(
+        X=df_data_4
+   ),
+)
+
+rm_columns5.fit(X=df_data_5)
+df_data_6 = pd.DataFrame.from_records(
+    data=rm_columns5.transform(
+        X=df_data_5
+   ),
+)
+
+rm_columns6.fit(X=df_data_6)
+df_data_7 = pd.DataFrame.from_records(
+    data=rm_columns6.transform(
+        X=df_data_6
+   ),
+)
+
+rm_columns7.fit(X=df_data_7)
+df_data_8 = pd.DataFrame.from_records(
+    data=rm_columns7.transform(
+        X=df_data_7
+   ),
+)
+
+rm_columns8.fit(X=df_data_8)
+df_data_9 = pd.DataFrame.from_records(
+    data=rm_columns8.transform(
+        X=df_data_8
+   ),
+)
+
+rm_columns9.fit(X=df_data_9)
+df_data_10 = pd.DataFrame.from_records(
+    data=rm_columns9.transform(
+        X=df_data_9
+   ),
+)
